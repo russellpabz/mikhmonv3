ed1cce35c64bb8997ed94db0a85bf4b8c23173bc
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-08-12 10:02:57 +0800

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,5 +1,6 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<julian','mikhmon>|>aWNlbA==');
+//$data['mikhmon'] = array ('1'=>'mikhmon<|<julian','mikhmon>|>aWNlbA==');
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
 
-$data['hotspot'] = array ('1'=>'hotspot!192.168.2.134','hotspot@|@julian','hotspot#|#aWNlbA==','hotspot%Tongoy Hotspot','hotspot^wifi.ph','hotspot&Rp','hotspot*10','hotspot(1','hotspot)','hotspot=disable','hotspot@!@disable');
+//$data['hotspot'] = array ('1'=>'hotspot!192.168.2.134','hotspot@|@julian','hotspot#|#aWNlbA==','hotspot%Tongoy Hotspot','hotspot^wifi.ph','hotspot&Rp','hotspot*10','hotspot(1','hotspot)','hotspot=disable','hotspot@!@disable');
