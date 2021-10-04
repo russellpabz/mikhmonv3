8f674fe26dcc37b1c433436340db5863171f115c
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-10-05 01:31:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ require_once("include/formatbytesbites.php");
 require_once("include/routeros_api.class.php");
 
 
-if(!isset($_SESSION["login"])){
-  //header("Location:". url("/?page=login"));
+if(!isset($_SESSION["login"]) && $_GET["page"] != "login"){
+    header("Location:". url("/?page=login"));
 }
 
 $API = new RouterosAPI(); 
