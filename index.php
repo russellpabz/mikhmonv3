c1c3037898dbe54816535f04deadf2b1c75416ae
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-10-06 20:57:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -43,11 +43,12 @@ $page = isset($_GET["page"]) ? trim($_GET["page"]) : "login";
 $connection = false;
 
  
-if(!isset($_SESSION["login"]) && $page != "login"){
-    header("Location:". url("/?page=login"));
-}
-else if(!$API->connect($config->mikrotik->ip_address, $config->mikrotik->user, decrypt($config->mikrotik->password)) && $page != "settings"){
+
+if(!$API->connect($config->mikrotik->ip_address, $config->mikrotik->user, decrypt($config->mikrotik->password)) && $page != "settings"){
    header("Location:". url("/?page=settings"));
+}
+else if(!isset($_SESSION["login"]) && $page != "login"){
+    header("Location:". url("/?page=login"));
 }
 
 $connection = true;
