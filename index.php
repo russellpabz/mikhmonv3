04bd8d63c33c456bf45e6d225e4c22fc8471c18c
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-10-12 19:18:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $API = new RouterosAPI();
 
 $page = isset($_GET["page"]) ? trim($_GET["page"]) : "login";
 $connection = false;
-
+die();
  
 if(!isset($_SESSION["login"]) || $_SESSION["login"] == false){
     $_SESSION["login"] = false;
@@ -54,7 +54,7 @@ else if(!$API->connect($config->mikrotik->ip_address, $config->mikrotik->user, d
     header("Location:". url("/?page=settings"));
 }
 
-die();
+
 
 $connection = true;
 
