f10034d46f5253366d6af7e2976f158648e0c755
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-10-12 22:42:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $connection = $API->connect($config->mikrotik->ip_address, $config->mikrotik->us
 
 
 if((!isset($_SESSION["login"]) || $_SESSION["login"] == '' || $_SESSION["login"] == false)  && $page != "login"){
-   // header("Location:". url("/?page=login"));
+   header("Location:". url("/?page=login"));
 }
 else if($connection == false && $page != "settings"){
    header("Location:". url("/?page=settings"));
