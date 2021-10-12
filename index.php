6b9bf269b089f7f73d8bffc3f3bc69a0382eea95
russellpabz <82968572+russellpabz@users.noreply.github.com>
2021-10-13 01:24:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -94,9 +94,9 @@ else{
    if((!isset($_SESSION['login']) && $_SESSION['login'] == '') && $page != "login"){
       header("Location: ". url("/?page=login"));
    }
-   else if(($connection == false || $connection == "") && $page != "settings"){
-      header("Location: ". url("/?page=settings"));
-   }
+   // else if(($connection == false || $connection == "") && $page != "settings"){
+   //    header("Location: ". url("/?page=settings"));
+   // }
 }
 
 
